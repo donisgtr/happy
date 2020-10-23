67c61a8a9893c622ee6fa7f10bb13a1902cf4150
donissousa <donisgtr@gmail.com>
2020-10-22 23:23:40 -0300

    initial backend

--- src/server.js
+++ src/server.js
@@ -1,1 +1,23 @@
-console.log("teste")
+//Importantando a dependencia
+const express = require('express');
+const path = require('path'); /* isso já e do Node */
+
+//Iniciando o Express
+const server = express();
+
+server
+    /* utilizando arquivos estaticos */
+    .use(express.static('public'))
+    
+    /* configurar template engine */
+    .set('views', path.join(__dirname, "view"))
+    .set('views engine', 'hbs')
+
+    // Criar uma Rota e/ou Caminho
+    .get('/', (request, response) => {
+        /* vai contanecar o texto de dirname com o outro */
+        return response.sendFile(path.join(__dirname, 'views', 'index.html'));
+    });
+
+// Ligar o servidor
+server.listen(5500);
